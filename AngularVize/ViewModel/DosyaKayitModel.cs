a6f633d69525bf16d957a0f0287b4e3879c7a5f3
semihoz11 <73099871+semihoz11@users.noreply.github.com>
2022-04-30 05:47:22 +0300

    Initial commit

---
+++ AngularVize/ViewModel/DosyaKayitModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularVize.ViewModel
+{
+    public class DosyaKayitModel
+    {
+        public string dkId { get; set; }
+        public string dkDersId { get; set; }
+        public string dkDosyaId { get; set; }
+    }
+}
---
+++ AngularVize/ViewModel/OgrenciModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularVize.ViewModel
+{
+    public class OgrenciModel
+    {
+        public string ogrId { get; set; }
+        public string ogrNo { get; set; }
+        public string ogrAdsoyad { get; set; }
+        public int ogrYas { get; set; }
+    }
+}
